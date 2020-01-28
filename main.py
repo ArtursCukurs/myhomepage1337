11347923c6c31e02da0e5bea57a15ad674bd97bf
[Students] Arturs Georgs  Cukurs <81DAGCukurs@rvt.lv>
2020-01-28 12:26:31 +0200

    d21-2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
@@ -9,7 +9,16 @@ def home():
 def getAbout():
   return render_template('about.html')
 
+@app.route('/params')
+def params():
+  return request.args 
+
+@app.route('/post_req', methods = ['POST'])
+def post_req():
+  return request.args  
+
 @app.route("/contact")
 def contact():
   return render_template('contact.html', phone = 87654321)  
 app.run(host='0.0.0.0', port=8020, threaded=True, debug=True)
+
